7fd806b37c55805a82e6a24dafa905284f4e9abd
Jonathan Pool <pool@stulta.com>
2017-08-16 12:09:43 -0700

    Added clarifying comment. Corrected style discrepancies found by linting.

--- public/script.js
+++ public/script.js
@@ -37,43 +37,42 @@ $(document).ready(function() {
   const texts = window.texts;
   textInit(texts);
   let currentBook;
-  $(".createButton").click(function() {
-    //get info from form, instead of hardcoded
-     addOne("Nexus", "Ramez Naam", "http://modernmrsdarcy.com/wp-content/uploads/2013/07/best-book.png", "April 25, 2020");
+  $('.createButton').click(function() {
+    // get info from form, instead of hardcoded
+    addOne('Nexus', 'Ramez Naam', 'http://modernmrsdarcy.com/wp-content/uploads/2013/07/best-book.png', 'April 25, 2020');
   });
-  // $(".specific").click(function() {
+  // $('.specific').click(function() {
   //   //placeholder
   // });
-  $(".editButton").click(function() {
+  $('.editButton').click(function() {
     // item text gets replaced with the pre-populated, editable form.
     // get specific id from the button click
-    getOne("599362ca5113cf0011283334");
+    getOne('599362ca5113cf0011283334');
     // edit button becomes a save button
   });
-  $(".saveButton").click(function() {
+  $('.saveButton').click(function() {
     // make this function get filled in by what I get from the form
-    updateOne("599362ca5113cf0011283334", 'title', 'Les Superhappy');
+    updateOne('599362ca5113cf0011283334', 'title', 'Les Superhappy');
     // once success message comes back from the server, input gets replaced with the updated text
   });
-  $(".deleteButton").click(function() {
+  $('.deleteButton').click(function() {
     // get specific id
-    deleteOne("599236cdbc824300112668b3");
+    deleteOne('599236cdbc824300112668b3');
   });
 
-
-// the AJAX call functions
-  let getAll = function() {
+  // the AJAX call functions
+  const getAll = function() {
     $.ajax('http://mutably.herokuapp.com/books')
-    .done(data=> {
+    .done(data => {
       // show all of them
-      data.books.forEach(el=> {console.log(el);});
+      data.books.forEach(el => {console.log(el);});
     })
     .fail(err => {
-      console.error("ERROR: \n" + err.status + "  " + err.statusText + " : " + err.responseText);
+      console.error('ERROR: \n' + err.status + '  ' + err.statusText + ' : ' + err.responseText);
     });
   };
 
-  let addOne = function(title, author, image, date) {
+  const addOne = function(title, author, image, date) {
     $.ajax({
       url: 'http://mutably.herokuapp.com/books',
       method: 'post',
@@ -84,35 +83,35 @@ $(document).ready(function() {
         releaseDate: date
       }
     })
-    .done(data=> {
+    .done(data => {
       // append this one to the bottom of the list of books
-      console.log(JSON.stringify(data.title) + " has been added");
+      console.log(JSON.stringify(data.title) + ' has been added');
     })
     .fail(err => {
-      console.error("ERROR: \n" + err.status + "  " + err.statusText + " : " + err.responseText);
+      console.error('ERROR: \n' + err.status + '  ' + err.statusText + ' : ' + err.responseText);
     });
   };
 
-  let getOne = function(id) {
+  const getOne = function(id) {
     $.ajax(`http://mutably.herokuapp.com/books/${id}`)
     .done(bookObject => {
       currentBook = bookObject;
       // show this one (with the button for editing+)
     })
     .fail(err => {
-      console.error("ERROR: \n" + err.status + "  " + err.statusText + " : " + err.responseText);
+      console.error('ERROR: \n' + err.status + '  ' + err.statusText + ' : ' + err.responseText);
     });
   };
 
-  let updateOne = function(id, thingToUpdate, replacementValue) {
+  const updateOne = function(id, thingToUpdate, replacementValue) {
     $.ajax({
       url: `http://mutably.herokuapp.com/books/${id}`,
       method: 'get',
     })
     .done(bookFromServerAsObject => {
       // Object.entries is an array of arrays of keys and values for that obj
-      let currentBookAsArrayToCompare = Object.entries(currentBook);
-      let bookFromServerAsArrayToCompare = Object.entries(bookFromServerAsObject);
+      const currentBookAsArrayToCompare = Object.entries(currentBook);
+      const bookFromServerAsArrayToCompare = Object.entries(bookFromServerAsObject);
       if(currentBookAsArrayToCompare.toString() === bookFromServerAsArrayToCompare.toString()) {
         currentBook[thingToUpdate] = replacementValue;
         $.ajax({
@@ -120,26 +119,26 @@ $(document).ready(function() {
           method: 'put',
           data: currentBook
         });
-        console.log(thingToUpdate + " was updated");
+        console.log(thingToUpdate + ' was updated');
       }
       else {
-        $("#revalert").html("Uh-oh, looks like someone else changed something already. Take a look at what it says now.");
-        console.log("something's wrong");
+        $('#revalert').html('Uh-oh, looks like someone else changed something already. Take a look at what it says now.');
+        console.log('something’s wrong');
       }
     })
     .fail(err => {
-      console.error("ERROR: \n" + err.status + "  " + err.statusText + " : " + err.responseText);
+      console.error('ERROR: \n' + err.status + '  ' + err.statusText + ' : ' + err.responseText);
     });
   };
 
-  let deleteOne = function(id) {
+  const deleteOne = function(id) {
     $.ajax({
       url: `http://mutably.herokuapp.com/books/${id}`,
       method: 'delete'
     })
-    .done(data => {console.log("book #" + id + " was successfully deleted");})
+    .done(() => {console.log('book #' + id + ' was successfully deleted');})
     .fail(err => {
-      console.error("ERROR: \n" + err.status + "  " + err.statusText + " : " + err.responseText);
+      console.error('ERROR: \n' + err.status + '  ' + err.statusText + ' : ' + err.responseText);
     });
   };
 });
